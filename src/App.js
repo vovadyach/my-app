e7b89ed7b9f44742c179c6b7e3c5ee325886af9a
vvd <vvd@norse.digital>
2018-03-14 16:48:01 +0200

    Ejects configs and scripts and adds css module options for both dev and prod.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Person from './Person/Person';
-import './App.css';
+import classes from './App.css';
 
 class App extends Component {
 
@@ -76,18 +76,18 @@ class App extends Component {
       style.backgroundColor = 'red';
     }
 
-    const classes = [];
+    const assignedClasses = [];
     if (this.state.persons.length <= 2) {
-      classes.push('red');
+      assignedClasses.push( classes.red );
     }
     if (this.state.persons.length <= 1) {
-      classes.push("bold");
+      assignedClasses.push( classes.bold );
     }
 
     return (
-      <div className="App">
+      <div className={classes.App}>
         <h1> Hi, I am a react app</h1>
-        <p className={classes.join(' ')}>This is really working</p>
+        <p className={assignedClasses.join(' ')}>This is really working</p>
         <button
           style={style}
           onClick={this.togglePersons}>Toggle Persons</button>
