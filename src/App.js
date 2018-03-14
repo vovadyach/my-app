7e6adc0d4f452b209d29b7d5bf1dd22b3ea1605c
vvd <vvd@norse.digital>
2018-03-14 14:15:32 +0200

    Adds flexible list.

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ class App extends Component {
 
   state = {
     persons: [
-      { name: "Vova", age: 30 },
-      { name: "Slava", age: 33 },
-      { name: "Lena", age: 32 }
+      { id: 'aa1', name: "Vova", age: 30 },
+      { id: 'bb2', name: "Slava", age: 33 },
+      { id: 'cc3', name: "Lena", age: 32 }
     ],
     showPersons: false
   }
@@ -17,18 +17,25 @@ class App extends Component {
   deletePersonHandler = (personIndex) => {
     const persons = [...this.state.persons];
     persons.splice(personIndex, 1);
-    
-    this.setState({ persons: persons })
+    this.setState({ persons: persons });
   }
 
-  nameChangeHandler = (event) => {
-    this.setState({
-      persons: [
-        { name: "Vova", age: 30 },
-        { name: event.target.value, age: 33 },
-        { name: "Lena", age: 34 }
-      ]
-    })
+  nameChangeHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    // const person = this.state.persons[personIndex];
+    const person = {
+      ...this.state.persons[personIndex]
+    }
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({ persons: persons })
   }
 
   togglePersons = () => {
@@ -54,10 +61,12 @@ class App extends Component {
       persons = (
         <div>
           {this.state.persons.map((person, index) => {
-            return <Person 
+            return <Person
               click={() => this.deletePersonHandler(index)}
               name={person.name}
-              age={person.age} />
+              age={person.age}
+              key={person.id}
+              changed={(event) => this.nameChangeHandler(event, person.id)} />
           })}
         </div>
       );
