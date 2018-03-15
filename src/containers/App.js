3e908c187edfc5dbd19ec3e401ef0fa5f94d3e14
vvd <vvd@norse.digital>
2018-03-15 12:55:37 +0200

    Converts statless component to stateful.

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,6 +4,10 @@ import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        console.log('[App.js] Inside Conctructor', props);
+    }
 
     state = {
         persons: [
