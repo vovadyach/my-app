1601d305e0113c005dda85856d6c48bcbb83bc39
vvd <vvd@norse.digital>
2018-03-16 14:10:49 +0200

    Adds withClass Wrapper HOC and anonymous class.

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import classes from './Person.css';
+import Aux from '../../../hoc/Aux';
+import withClass from '../../../hoc/withClass';
 
 class Person extends Component {
     constructor(props) {
@@ -20,16 +22,16 @@ class Person extends Component {
     render() {
         console.log('[Person.js] Inside render()');
         return (
-            <div className={classes.Person}>
+            <Aux>
                 <p onClick={this.props.click}>I am a {this.props.name} and I am {this.props.age} years old!!!</p>
                 <p>{this.props.children}</p>
                 <input type="text" 
                     ref={(ipt) => {this.inputElm = ipt} }
                     onChange={this.props.changed} 
                     value={this.props.name} />
-            </div>
+            </Aux>
         );
     }
 }
 
-export default Person;
+export default withClass(Person, classes.Person);
